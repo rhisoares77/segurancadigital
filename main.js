1bd32bbda7009b6fabd97046f32c417d095968bf
rhi soares <168201929+rhisoares77@users.noreply.github.com>
2026-06-22 08:56:16 -0300

    .

--- main.js
+++ main.js
@@ -76,3 +76,9 @@ function classificaSenha(tamanhoAlfabeto){
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
 
+
+
+
+
+
+
